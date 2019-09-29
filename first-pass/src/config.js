6e7be60e86bdcfc15f936cfad29fd25c97119b2d
Andy Wang <andy.wang@blockchaintech.net.au>
2019-09-29 21:53:32 +1000

    Finish chapter1

--- first-pass/src/config.js
+++ first-pass/src/config.js
@@ -1,6 +1,19 @@
+const createDb = require('./db');
+const createHomeApp = require('./app/home');
+const createRecordViewingsApp = require('./app/record-viewings');
+
 function createConfig({ env }) {
+  const db = createDb({
+    connectionString: env.databaseUrl
+  });
+  const homeApp = createHomeApp({ db });
+  const recordViewingsApp = createRecordViewingsApp({ db });
+
   return {
     env,
+    db,
+    homeApp,
+    recordViewingsApp,
   }
 }
 
